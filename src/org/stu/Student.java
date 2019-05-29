a08988a1e6f42064adeec6c551c1f1c11a4114c0
balak56 <balakrish2205@gmail.com>
2019-05-29 19:53:35 +0530

    Welcome to git

---
+++ src/org/stu/Student.java
@@ -0,0 +1,29 @@
+package org.stu;
+
+import java.util.Scanner;
+
+public class Student {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your id-");
+		int studentId=sc.nextInt();
+		System.out.println("Enter your Name-");
+		String studentName=sc.next();
+		int Mark1=90;
+		int Mark2=95;
+		int Mark3=80;
+		int Mark4=85;
+		int Mark5=70;
+		System.out.println("Studentid:"+studentId);
+		System.out.println("StudentName:"+studentName);
+		System.out.println("Mark1:"+Mark1);
+		System.out.println("Mark2:"+Mark2);
+		System.out.println("Mark3:"+Mark3);
+		System.out.println("Mark4:"+Mark4);
+		System.out.println("Mark5:"+Mark5);
+		System.out.println("Total:"+(Mark1+Mark2+Mark3+Mark4+Mark5));
+		System.out.println("Average:"+(Mark1+Mark2+Mark3+Mark4+Mark5)/5);
+						
+	}
+}
